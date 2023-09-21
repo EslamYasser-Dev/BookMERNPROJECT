d9335e2c3ddee270a12438d373f7b975b4569eb4
Eslam <e.yasser997@gmail.com>
2023-09-21 10:04:37 +0300

    fix CORS policy error (allow custom origins

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
-import  express, { request, response } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
@@ -12,20 +13,35 @@ const app = express();
 //     return response.status(234).send("hello");
 // });
 
+//middleware for parsing requset body
+app.use(express.json());
+
+//===================================================================
+//middleware to handle cors policy
+//To allow all origins with defualt of cors
+// app.use(cors());
+
+//allow custom origins 
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['content-type'],
+})
+);
+//==================================================================
 //port
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App listening to port: ${PORT}`);
 });
 
 //mongo DB conncetion
-mongoose.connect(mongoDBURL).then(()=>{
+mongoose.connect(mongoDBURL).then(() => {
     console.log("database connection success");
 })
-.catch((error)=>{
-    console.log(error);
-});
-app.use(express.json());
-app.use('/books',booksRoute);
+    .catch((error) => {
+        console.log(error);
+    });
+app.use('/books', booksRoute);
 
 //route to add new book(send data to mongo DB)
 
--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -3,19 +3,19 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/', async (request,response)=>{
+router.post('/', async (request, response) => {
     try {
         //ensure that all fields are not null
-        if(!request.body.title||!request.body.author||!request.body.publishYear){
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
             return response.status(400).send({
-                message:'Send all required info fields: Title, Auther and Publish Year',
+                message: 'Send all required info fields: Title, Auther and Publish Year',
             });
         }
         //save the data in an Object 
         const newBook = {
             title: request.body.title,
             author: request.body.author,
-            publishYear:request.body.publishYear
+            publishYear: request.body.publishYear
         }
 
         //save the data(newBook) in a variable called book from Book type 
@@ -27,75 +27,76 @@ router.post('/', async (request,response)=>{
 
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({message: error.message})
+        response.status(500).send({ message: error.message })
     }
 });
 
 
 //Route to Get All Books from Database(mongo)
-router.get('/', async(request, response)=>{
+router.get('/', async (request, response) => {
     try {
         const books = await Book.find({});
         return response.status(200).json({
-            count:books.length, //this to return count of books inside object {count:5,data:{....}}
-            data:books,
+            count: books.length, //this to return count of books inside object {count:5,data:{....}}
+            data: books,
         });
-        
+
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({message:error.message});
+        response.status(500).send({ message: error.message });
     }
 });
 
 
 //Method to get spicefic book (search)
-router.get('/:id', async(request, response)=>{
+router.get('/:id', async (request, response) => {
     try {
-        const {id} = request.params;
+        const { id } = request.params;
 
         const book = await Book.findById(id); // we used params here 
         return response.status(200).json(book);
-       
+
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({message:error.message});
+        response.status(500).send({ message: error.message });
     }
 });
 
 //route to update a book
-router.put('/:id', async(request, response)=>{
+router.put('/:id', async (request, response) => {
     try {
-        if(!request.body.title||!request.body.author||!request.body.publishYear){
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
             return response.status(400).send({
-                message:'Send all required info fields: Title, Auther and Publish Year',
+                message: 'Send all required info fields: Title, Auther and Publish Year',
             });
         }
-            const {id} = request.params;
-            const result =await Book.findByIdAndUpdate(id, request.body);
-            if(!result){
-                return response.status(404).json({message:'Book not found'})
-            }
-            return response.status(200).send({message:'Book has been Updated Successfully'})
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' })
         }
+        return response.status(200).send({ message: 'Book has been Updated Successfully' })
+    }
     catch (error) {
         console.log(error.message);
-        response.status(500).send({message:error.message});
+        response.status(500).send({ message: error.message });
     }
 });
 
 //Route to Delete a book
-router.delete('/:id',async(request,response)=>{
+router.delete('/:id', async (request, response) => {
     try {
-        const {id}= request.params;
+        const { id } = request.params;
         const result = await Book.findByIdAndDelete(id);
-        if(!result){return response.status(404).send({message:"book is not found"}); 
-    }
-        return response.status(200).send({message:"Book Deleted Successfully"});
-        
+        if (!result) {
+            return response.status(404).send({ message: "book is not found" });
+        }
+        return response.status(200).send({ message: "Book Deleted Successfully" });
+
 
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({message:error.message}); 
+        response.status(500).send({ message: error.message });
     }
 });
 
